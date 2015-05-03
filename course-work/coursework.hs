7bad109b0185dcc3901e854a2d075950330cab6e
rikujjs <marttila.riku@gmail.com>
2015-05-03 19:06:30 +0300

    Refactoring, grouping and adding comments

--- course-work/coursework.hs
+++ course-work/coursework.hs
@@ -11,10 +11,9 @@ main = wordCounter `catch` handler
 
 wordCounter = do
     args <- getArgs
-    file1 <- readFile (args !! 1) -- String
+    file1 <- readFile (args !! 1)
     file2 <- readFile (args !! 2)
   
-
     let command = (args !! 0)
 
     if command == "first"
@@ -26,70 +25,76 @@ wordCounter = do
           else
             putStrLn "Not implemented"
 
--- Take file content and turn into parsed data (sentences in arrays containing words in array)
-fileParser :: String -> [[String]]
-fileParser fileContent = toWords (toSentences fileContent)
-
-
-checkParam :: String -> String
-checkParam param
-  | param == "first" = "Operation first"
-  | param == "second" = "Operation second"
-  | param == "third" = "Operation third"
-  | otherwise =  "Not implemented"
-
 handler :: IOError -> IO ()  
 handler e  
     | isDoesNotExistError e = putStrLn "The input file doesn't exist!"
     | otherwise = ioError e
 
-distanceMax :: [[String]] -> [[String]] -> Int
-distanceMax xs ys = sum [sentenceDist xss yss | xss <- xs, yss <- ys]
+-- Take file content and turn into parsed data (sentences in arrays containing words in array)
+-- We want to strip commas and dots, then group by separators, and filter them out in the end
+-- The following section contains helpers for this.
+fileParser :: String -> [[String]]
+fileParser fileContent = toWords (toSentences (filterControls fileContent))
 
-distanceMin :: [[String]] -> [[String]] -> Int
-distanceMin xs ys = minimum $ map (\x -> tupleSum x) (candidateSents xs ys)
-
-tupleSum :: [(Int,String,String)] -> Int
-tupleSum xs = sum [a | (a,b,c) <- xs]
-
-minSum :: [[String]] -> [[String]] -> Int
-minSum xs ys = minimum $ map (\x -> tupleSum x) (candidateSents xs ys)
-
--- Returns the first (in order) solution that has the minimum sum of differences!
-minimumFilter :: [[String]] -> [[String]] -> [(Int, String, String)]
-minimumFilter xs ys = head (dropWhile (\x -> (tupleSum x) > (minSum xs ys)) (candidateSents xs ys))
-
-tupleToString :: (Int, String, String) -> String
-tupleToString (a, b, c) = b ++ " & " ++ c 
-
-threeTuplePrinter :: [(Int, String, String)] -> IO ()
-threeTuplePrinter tupleList = putStrLn . unlines . map tupleToString $ tupleList
-
-candidateSents :: [[String]] -> [[String]] -> [[(Int, String, String)]]
-candidateSents xs ys = [[(sentenceDist xss (ys !! a), unwords xss, unwords (ys !! a)) | xss <- xs] | a <- [0..(length ys - 1)]]
-
-sentenceDist :: [String] -> [String] -> Int
-sentenceDist xs ys = length $ (xs \\ ys) `union` (ys \\ xs)
+filterControls :: String -> String
+filterControls input = filter (not . isControl) input
 
 toWords :: [String] -> [[String]]
 toWords input = map words $ input
 
 toSentences :: String -> [String]
-toSentences input = filter (not . any isSprtr) . groupBy ((==) `on` isSprtr) $ map toLower (stripCommas input)
+toSentences input = filter (not . any isSeparator2) . groupBy ((==) `on` isSeparator2) $ map toLower (stripCommas input)
 
 stripCommas :: String -> String
-stripCommas input = filter (not . isCommaOrDdot) input
+stripCommas input = filter (not . isCommaDot) input
 
-isCommaOrDdot :: Char -> Bool
-isCommaOrDdot ch
+isCommaDot :: Char -> Bool
+isCommaDot ch
   | ch == ',' = True
   | ch == ':' = True
   | otherwise = False
 
-isSprtr :: Char -> Bool
-isSprtr ch
+isSeparator2 :: Char -> Bool
+isSeparator2 ch
     | ch == '.' = True
     | ch == ';' = True
     | ch == '!' = True
     | ch == '?' = True
     | otherwise = False
+
+-- Function to calculate distance with first method
+distanceMax :: [[String]] -> [[String]] -> Int
+distanceMax xs ys = sum [sentenceDistance xss yss | xss <- xs, yss <- ys]
+
+sentenceDistance :: [String] -> [String] -> Int
+sentenceDistance xs ys = length $ (xs \\ ys) `union` (ys \\ xs)
+
+-- Second method calculation. Finds the first sentence pairs with the smallest distance.
+-- When the group of sentece pairs with the smallest distance is found, general helper functions
+-- are used to print them according to the specification.
+--
+-- The output is a list of three tuples (Distance, first sentence, second sentence), which contain
+-- sentence pairs and their distances (the smallest possible pairs)
+minimumFilter :: [[String]] -> [[String]] -> [(Int, String, String)]
+minimumFilter xs ys = head (dropWhile (\x -> (tupleSum x) > (minSum xs ys)) (candidateLists xs ys))
+
+-- Calculate total distance of sentences for the "candidate list"
+tupleSum :: [(Int,String,String)] -> Int
+tupleSum xs = sum [a | (a,b,c) <- xs]
+
+-- Calculates the smallest sum of all possible candidate lists
+minSum :: [[String]] -> [[String]] -> Int
+minSum xs ys = minimum $ map (\x -> tupleSum x) (candidateLists xs ys)
+
+-- Returns all possible combinations of sentence pairs, from this list, the combination with the
+-- smallest sum is calculated
+candidateLists :: [[String]] -> [[String]] -> [[(Int, String, String)]]
+candidateLists xs ys = [[(sentenceDistance xss (ys !! a), unwords xss, unwords (ys !! a)) | xss <- xs] | a <- [0..(length ys - 1)]]
+
+-- Helper functions for printing a three-tuple in the way stated in the course work
+-- specification.
+tupleToString :: (Int, String, String) -> String
+tupleToString (a, b, c) = b ++ " & " ++ c 
+
+threeTuplePrinter :: [(Int, String, String)] -> IO ()
+threeTuplePrinter tupleList = putStrLn . unlines . map tupleToString $ tupleList
